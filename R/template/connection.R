b8a4ba8c6ac1d2c81872dacff2554c093b9a7141
sidohaakma <sido@haakma.org>
2018-10-15 20:17:20 +0200

    refactor reposirtories and moved some files to anlaysis-protocols

--- R/template/connection.R
+++
@@ -1,9 +0,0 @@
-# connect to opal instances
-server <- c("opal-instance1", "opal-instance1")
-url <- c("https://opal1.domain.org", "https://opal2.domain.org")
-username <- c("usr1", "usr2")
-password <- c("pw1", "pw2")
-table <- c("Porject1.table1", "Project2.table2")
-logindata <- data.frame(server,url,username,password,table)
-datashield.logout(opals)
-opals <- datashield.login(logins=logindata,assign=TRUE)
